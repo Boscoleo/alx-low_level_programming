9061c9823aad555ad33097dc576c7c9da0aff1cd
boscoleo <boscoleo54@gmail.com>
2023-03-20 22:13:09 -0700

    hello, world

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,10 +3,12 @@
 /**
  * main - print the size of various types
  *
- * Description: prints the size of various types on the computer it is compiled and run on
+ * Description: prints the size of various types on the computer it is
+ * and run on
  *
  * Return: Always 0 (Success)
  */
+
 
 int main(void)
 {
